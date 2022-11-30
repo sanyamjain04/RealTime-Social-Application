c68d310ea6b76113ff3057e345835add9c3ba388
Your Namesanyamjain04 <sanyamjainsj04@gmail.com>
2022-11-30 12:28:34 +0530

    created settings functionality

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,5 +1,10 @@
+import clsx from "clsx"
+import dynamic from "next/dynamic"
 import React, { ReactNode } from "react"
 import Sidebar from "../components/sidebar/Sidebar"
+import { useThemeDirection } from "../zustand/themeDirection"
+
+const Settings = dynamic(()=>import('../components/settings'),{ssr:false})
 
 type LayoutProps = {
     children : ReactNode
@@ -7,10 +12,12 @@ type LayoutProps = {
 
 
 const Layout :React.FC<LayoutProps> = ({children}) => {
+  const reverseDirection = useThemeDirection(state=>state.reverseDirection)
   return (
-    <div className="flex w-screen h-screen dark:bg-dark-secondary dark:text-white">
+    <div className={clsx("flex w-screen h-screen dark:bg-dark-secondary dark:text-white",reverseDirection && 'flex-row-reverse')}>
         <Sidebar />
         {children}
+        <Settings />
     </div>
   )
 }
---
+++ src/zustand/themeDirection.ts
@@ -0,0 +1,13 @@
+import create from "zustand"
+
+interface ThemeDirectionProps {
+    reverseDirection:boolean
+    changeDirection:(prop:boolean)=>void
+}
+
+export const useThemeDirection = create<ThemeDirectionProps>()(
+        (set, get) => ({
+            reverseDirection: false,
+            changeDirection:(prop:boolean)=>set({reverseDirection: prop})
+        }),
+)
