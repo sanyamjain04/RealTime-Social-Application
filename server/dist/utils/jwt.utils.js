83a0616c021f0ad4c53a341daf2ac6627c5b6b13
Sanyam Jain <sanyamjainsj04@gmail.com>
2023-02-01 13:28:50 +0530

    removed dist direactory in backend

--- server/dist/utils/jwt.utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.signToken = void 0;
-const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-const signToken = (userId) => jsonwebtoken_1.default.sign({ userId }, process.env.JWT_SECRET);
-exports.signToken = signToken;
